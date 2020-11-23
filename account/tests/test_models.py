6eacc292572f931533e9faa796ca2e70b3d46027
surajit <surajit@poweredbypeople.io>
2020-11-23 18:05:55 +0300

    added profile model

--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -5,5 +5,5 @@ pytestmark = pytest.mark.django_db
 
 class TestUserProfile:
     def test_user_profile(self):
-        obj = mixer.blend("account.UserProfile")
+        obj = mixer.blend("account.Profile")
         assert obj.pk == 1, "Should create a UserProfile instance"
