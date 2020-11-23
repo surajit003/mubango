6089f1480d341f1a8e3638c560cc90de9317aba0
surajit <surajit@poweredbypeople.io>
2020-11-23 21:55:25 +0300

    added state model

--- account/models.py
+++ account/models.py
@@ -6,10 +6,16 @@ from django_countries.fields import CountryField
 class Country(models.Model):
     country = CountryField()
 
+
+class State(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, db_index=True)
+    name = models.CharField(max_length=50, db_index=True)
+
+    class Meta:
+        unique_together = ("country", "name")
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    verification_id = models.CharField(max_length=120,unique=True,db_index=True)
+    verification_id = models.CharField(max_length=120, unique=True, db_index=True)
     birth_date = models.DateField(null=True, blank=True)
-
-
-
