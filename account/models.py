f43dda2ab61e1e4271f484ee45b3bfddb081e39c
surajit <surajit@poweredbypeople.io>
2020-11-23 21:49:51 +0300

    added country model

--- account/models.py
+++ account/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
+
+class Country(models.Model):
+    country = CountryField()
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     verification_id = models.CharField(max_length=120,unique=True,db_index=True)
     birth_date = models.DateField(null=True, blank=True)
+
 
 
