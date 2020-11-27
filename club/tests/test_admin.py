62a69e1a32467efb1a696ab0e941c566bea5631f
surajit003 <surajit@poweredbypeople.io>
2020-11-27 23:14:22 +0300

    added test for get address function

---
+++ club/tests/test_admin.py
@@ -0,0 +1,19 @@
+import pytest
+from mixer.backend.django import mixer
+from django.contrib.admin.sites import AdminSite
+from .. import admin
+from .. import models
+
+pytestmark = pytest.mark.django_db
+
+
+class TestClubAdmin:
+    def test_get_address(self):
+        obj = mixer.blend("club.Club")
+        site = AdminSite()
+        club_admin = admin.ClubAdmin(models.Club, site)
+        result = club_admin.get_address(obj)
+        assert result == obj.address.location, "Should return the location value"
+
+        assert obj.pk == 1, "Should create a Club instance"
+        assert str(obj) == "{} {}".format(obj.name, obj.active)
