4ba658938da079e805106f5946f6f9cf060ce732
surajit003 <surajit@poweredbypeople.io>
2020-11-27 20:49:07 +0300

    add club app

---
+++ club/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+from common.models import Address
+
+
+class Club(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(blank=True, null=True)
+    phone_number = PhoneNumberField()
+    active = models.BooleanField(default=False)
+    address = models.ForeignKey(Address, on_delete=models.PROTECT)
+    price = models.CharField(
+        max_length=10, null=True, blank=True
+    )  # not actual price per se more like $$$ or $$
+
+    def __str__(self):
+        return "{} {}".format(self.name, self.active)
+
+    class Meta:
+        verbose_name_plural = "Clubs"
--- club/tests/test_models.py
+++ club/tests/test_models.py
@@ -4,13 +4,8 @@ from mixer.backend.django import mixer
 pytestmark = pytest.mark.django_db
 
 
-class TestUserProfile:
-    def test_profile(self):
-        obj = mixer.blend("account.Profile")
-        assert obj.pk == 1, "Should create a UserProfile instance"
-        assert str(obj) == "{} {}".format(obj.user.username, obj.active)
-
-    def test_follower(self):
-        obj = mixer.blend("account.Follower")
-        assert obj.pk == 1, "Should create a Follower instance"
-        assert str(obj) == "{} {}".format(obj.user, obj.follower)
+class TestClub:
+    def test_club(self):
+        obj = mixer.blend("club.Club")
+        assert obj.pk == 1, "Should create a Club instance"
+        assert str(obj) == "{} {}".format(obj.name, obj.active)
