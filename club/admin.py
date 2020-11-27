d9036e12f58174481add583dc7994ee43bbfd730
surajit003 <surajit@poweredbypeople.io>
2020-11-27 23:16:51 +0300

    added get address function

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "active",
+        "rating",
+        "get_address",
+    )
+    search_fields = ("name",)
+
+    def get_address(self, obj):
+        return obj.address.location
+
+    get_address.short_description = "Address"
