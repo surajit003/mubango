aa2a0a317742f115360372870d0b531d294f436b
surajit003 <surajit@poweredbypeople.io>
2020-12-04 13:28:03 +0300

    added test for trending model

--- club/tests/test_models.py
+++ club/tests/test_models.py
@@ -14,3 +14,7 @@ class TestClub:
         obj = mixer.blend("club.VisitorCount")
         assert obj.pk == 1, "Should create a VisitorCount instance"
         assert str(obj) == "{} {}".format(obj.club.name, obj.count)
+
+    def test_trending(self):
+        obj = mixer.blend("club.Trending")
+        assert obj.pk == 1, "Should create a Trending instance"
