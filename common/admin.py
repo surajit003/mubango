991b0802e206360621ac45125f6b387e087f732e
surajit003 <surajit@poweredbypeople.io>
2020-12-05 16:25:29 +0300

    added rating modeladmin

--- common/admin.py
+++ common/admin.py
@@ -43,3 +43,21 @@ class GuestAdmin(admin.ModelAdmin):
 class MusicGenreAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
+
+
+@admin.register(models.Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+    )
+    search_fields = ("name",)
+
+
+@admin.register(models.Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "symbol",
+    )
+    search_fields = ("name",)
