26d34673daeb72cdc78d346c8afd7d5e562eca6d
surajit003 <surajit@poweredbypeople.io>
2020-12-06 01:57:01 +0300

    modify test

--- event/tests/test_admin.py
+++ event/tests/test_admin.py
@@ -8,14 +8,14 @@ pytestmark = pytest.mark.django_db
 
 
 class TestEventAdmin:
-    def test_get_club_event(self):
+    def test_get_event_name(self):
         obj = mixer.blend("event.EventSetUp")
         site = AdminSite()
         event_setup_admin = admin.EventSetUpAdmin(models.EventSetUp, site)
         result = event_setup_admin.get_event(obj)
         assert result == obj.event.name, "Should return the event value"
 
-    def test_get_club_name(self):
+    def test_get_business_name(self):
         obj = mixer.blend("event.EventSetUp")
         site = AdminSite()
         event_setup_admin = admin.EventSetUpAdmin(models.EventSetUp, site)
