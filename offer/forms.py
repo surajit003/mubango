f8963f8014f8b71b15a3193db991f5c2277cabf8
surajit003 <surajit@poweredbypeople.io>
2020-12-19 23:07:52 +0300

    removed debug statement

--- offer/forms.py
+++ offer/forms.py
@@ -12,14 +12,10 @@ class OfferForm(forms.ModelForm):
         Override the default clean method to check whether this offer exists
         for a company.
         """
-        print("entttttttt")
         cleaned_data = self.cleaned_data
-        print("s", cleaned_data)
         code = cleaned_data.get("code")
         business = cleaned_data.get("business")
-        print("code", code, business)
         offer_code = Offer.objects.filter(code=code, business=business).exists()
-        print("offer", offer_code)
         if offer_code:
             raise forms.ValidationError(
                 "A code exists for another offer for this business"
