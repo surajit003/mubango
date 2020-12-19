7124b14a4ed05500a8b7cac2604beb1120a99f0b
surajit003 <surajit@poweredbypeople.io>
2020-12-19 15:27:45 +0300

    added offer admin

--- offer/admin.py
+++ offer/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "code",
+        "is_active",
+        "is_special",
+    )
+    search_fields = (
+        "title",
+        "code",
+        "business",
+    )
