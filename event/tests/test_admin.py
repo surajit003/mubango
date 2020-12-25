2560c28dc4606e4b4de8d3582a95ee6188314595
surajit003 <surajit@poweredbypeople.io>
2020-12-25 18:26:25 +0300

    modify test models

--- event/tests/test_admin.py
+++ event/tests/test_admin.py
@@ -3,21 +3,24 @@ from mixer.backend.django import mixer
 from django.contrib.admin.sites import AdminSite
 from .. import admin
 from .. import models
+from mock import patch
 
 pytestmark = pytest.mark.django_db
 
 
 class TestEventAdmin:
     def test_get_event_name(self):
-        obj = mixer.blend("event.EventSetUp")
+        event = mixer.blend("event.Event", num_of_tickets=10)
+        obj = mixer.blend("event.EventUser", event=event)
         site = AdminSite()
-        event_setup_admin = admin.EventSetUpAdmin(models.EventSetUp, site)
+        event_setup_admin = admin.EventUserAdmin(models.EventUser, site)
         result = event_setup_admin.get_event(obj)
         assert result == obj.event.name, "Should return the event name"
 
-    def test_get_business_name(self):
-        obj = mixer.blend("event.EventSetUp")
+    def test_get_user_name(self):
+        event = mixer.blend("event.Event", num_of_tickets=10)
+        obj = mixer.blend("event.EventUser", event=event)
         site = AdminSite()
-        event_setup_admin = admin.EventSetUpAdmin(models.EventSetUp, site)
-        result = event_setup_admin.get_business(obj)
-        assert result == obj.business.name, "Should return the business name"
+        event_user_admin = admin.EventUserAdmin(models.EventUser, site)
+        result = event_user_admin.get_user(obj)
+        assert result == obj.visitor.username, "Should return the username"
--- event/tests/test_models.py
+++ event/tests/test_models.py
@@ -1,5 +1,8 @@
 import pytest
 from mixer.backend.django import mixer
+from address.models import Address, Locality, State, Country
+from ..models import Event
+import datetime
 
 pytestmark = pytest.mark.django_db
 
@@ -11,6 +14,53 @@ class TestEvent:
         assert str(obj) == "{} {}".format(obj.name, obj.genre)
 
     def test_eventsetup(self):
-        obj = mixer.blend("event.EventSetUp")
-        assert obj.pk == 1, "Should create a EventSetUp instance"
-        assert str(obj) == "{} {}".format(obj.business.name, obj.event.name)
+        event = mixer.blend("event.Event", num_of_tickets=10)
+        obj = mixer.blend("event.EventUser", event=event)
+        assert obj.pk == 1, "Should create a EventUser instance"
+        assert str(obj) == "{} {}".format(obj.event.name, obj.visitor.username)
+
+    def test_event_user_for_no_available_tickets(self):
+        event = mixer.blend("event.Event", limit=0)
+        with pytest.raises(Exception):
+            mixer.blend("event.EventUser", event=event)
+
+
+class TestEventManager:
+    def test_event_manager_get_queryset(self):
+        country = Country.objects.create(name="Kenya", code="KE")
+        state = State.objects.create(name="Nairobi", country=country)
+        locality = Locality.objects.create(name="USIU", state=state)
+        address = Address.objects.create(
+            street_number="USIU Road Thika road",
+            locality=locality,
+            raw="USIU road",
+            latitude="-1.2211537",
+            longitude="36.88339089999999",
+        )
+        dt = datetime.date.today()
+
+        business = mixer.blend("business.Business", address=address)
+        event = Event.event_manager.create(
+            name="test",
+            to_be_held_on=dt,
+            active=True,
+            location=address,
+        )
+        event.business.add(business)
+        event.save()
+        assert event.id == 1
+
+        event = Event.event_manager.upcoming_events("Nairobi")
+        assert event.count() == 1
+
+        event = Event.event_manager.create(
+            name="test",
+            to_be_held_on=dt,
+            active=True,
+            location=address,
+            is_special=True,
+        )
+        event.business.add(business)
+        event.save()
+        ev = Event.event_manager.upcoming_special_events("Nairobi")
+        assert ev.count() == 1
