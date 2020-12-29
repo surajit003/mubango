bbdafd6532f186333cefcf908dc1c882dda459fe
surajit003 <surajit@poweredbypeople.io>
2020-12-30 00:20:44 +0300

    modify review model

--- review/models.py
+++ review/models.py
@@ -4,6 +4,23 @@ from common.models import DateModel
 from django.contrib.auth.models import User
 
 
+class ReviewManager(models.Manager):
+    def get_queryset(self):
+        return super(ReviewManager, self).get_queryset().filter(moderated=True)
+
+    def get_review_for_a_specific_user(self, username):
+        # get review for a specific user
+        return self.get_queryset().filter(
+            user__username=username,
+        )
+
+    def review_for_a_business(self, business):
+        # includes review for a business
+        return self.get_queryset().filter(
+            business__name=business,
+        )
+
+
 class Review(DateModel):
     level_of_experience = (
         ("positive", "Positive"),
@@ -15,9 +32,13 @@ class Review(DateModel):
     experience = models.CharField(
         max_length=20, choices=level_of_experience, default="neutral"
     )
-    rating = models.IntegerField(default=1)
+    ratings = models.IntegerField(default=1)
     images = models.ImageField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    moderated = models.BooleanField(default=False)
+
+    objects = models.Manager()  # The default manager.
+    review_manager = ReviewManager()  # The review manager.
 
     class Meta:
         verbose_name_plural = "Reviews"
@@ -25,3 +46,16 @@ class Review(DateModel):
 
     def __str__(self):
         return "{} {} {}".format(self.id, self.user.username, self.business.name)
+
+    def conv_star_to_rating(self, star_rating):
+        if star_rating == "*":
+            self.ratings = 1
+        elif star_rating == "**":
+            self.ratings = 2
+        elif star_rating == "***":
+            self.ratings = 3
+        elif star_rating == "****":
+            self.ratings = 4
+        elif star_rating == "*****":
+            self.ratings = 5
+        self.save()
