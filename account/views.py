077122b232c67174a183fd4f52fd3580ccbb3905
surajit003 <surajit@poweredbypeople.io>
2021-01-03 01:16:45 +0300

    modify view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+# Create your views here.
+def login(request):
+    return render(request, "account/login.html")
+
+
+def home(request):
+    return render(request, "account/home.html")
