d0906be8ad6535862c6023b7aaf7f4a0720fcde8
surajit003 <surajit@poweredbypeople.io>
2021-01-02 21:10:23 +0300

    add django social auth

--- Mubango/settings.py
+++ Mubango/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "social_django",
     "django_countries",
     "debug_toolbar",
     "phonenumber_field",
