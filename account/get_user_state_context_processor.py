32cb1cb255e17e9308a55e9c677cf9077ac7e2a2
surajit003 <surajit@poweredbypeople.io>
2021-01-10 21:42:48 +0300

    modify context processor

--- account/get_user_state_context_processor.py
+++ account/get_user_state_context_processor.py
@@ -2,6 +2,8 @@ from .models import Profile
 
 
 def get_user_state(request):
-    if request.user:
+    if request.user and not request.user.is_anonymous:
         profile = Profile.objects.get(user=request.user)
         return {"user_state": profile.get_state()}
+    else:
+        return {}
