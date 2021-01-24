abd323ee3782027fefb8f6807f12fa6ccd0aa6f5
surajit003 <surajit@poweredbypeople.io>
2021-01-25 01:19:39 +0300

    modify timezone

--- Mubango/settings.py
+++ Mubango/settings.py
@@ -155,13 +155,13 @@ AUTHENTICATION_BACKENDS = (
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Africa/Nairobi"
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
