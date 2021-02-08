eaaa9417ac788885507efffa2ba68dde3bb30f25
surajit003 <surajit@poweredbypeople.io>
2021-02-08 22:05:45 +0300

    remove redundant code

--- business/admin.py
+++ business/admin.py
@@ -14,21 +14,6 @@ class BusinessServiceLink(admin.StackedInline):
     model = models.BusinessServiceRating
 
 
-# @admin.register(models.Service)
-# class ServiceAdmin(admin.ModelAdmin):
-#     list_display = ("name",)
-
-
-# @admin.register(models.BusinessServiceRating)
-# class BusinessServiceAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "service",
-#         "rating",
-#         "business",
-#     )
-#     search_fields = ("business__name",)
-
-
 @admin.register(models.Business)
 class BusinessAdmin(admin.ModelAdmin):
     list_display = (
@@ -47,15 +32,3 @@ class BusinessAdmin(admin.ModelAdmin):
         return obj.address.raw
 
     get_address.short_description = "Address"
-
-
-#
-# @admin.register(models.VisitorCount)
-# class VisitorCountAdmin(admin.ModelAdmin):
-#     list_display = ("business", "count")
-#     search_fields = ("business",)
-#
-#
-# @admin.register(models.BusinessSocial)
-# class BusinessSocialAdmin(admin.ModelAdmin):
-#     list_display = ("business", "social", "url", "active")
