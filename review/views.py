0fd4772b5b010264e0abd3ee7bf60cce8ad15a48
surajit003 <surajit@poweredbypeople.io>
2021-03-25 20:17:39 +0300

    modify ajax

--- review/views.py
+++ review/views.py
@@ -7,13 +7,24 @@ from business.models import Business
 
 def save_review(request):
     if request.method == "POST" and request.is_ajax():
+        print("req", request.POST, request.FILES)
+        image1 = request.FILES.get("image_1")
+        image2 = request.FILES.get("image_2")
+        image3 = request.FILES.get("image_3")
+        print("im", image1, image2, image3)
         comment = request.POST.get("comment")
         experience = request.POST.get("experience")
         user = request.user
         b_slug = request.POST.get("business_slug")
         business = Business.objects.get(slug=b_slug)
         Review.objects.create(
-            business=business, user=user, comment=comment, experience=experience
+            business=business,
+            user=user,
+            comment=comment,
+            experience=experience,
+            image_1=image1,
+            image_2=image2,
+            image_3=image3,
         )
         data = {"status": 201, "response": "Review Saved Successfully"}
         return JsonResponse(data, safe=False)
--- static/js/review.js
+++ static/js/review.js
@@ -1,24 +1,39 @@
 $('#btn_review').click(function (e) {
     e.preventDefault()
-    comment = $('#comment').val();
+    $('#review_status').empty()
     experience = $('#experience').val();
-    csrf_token = $("input[name=csrfmiddlewaretoken]").val()
-    business_slug = $('#business_slug').val()
-    $('#review_status').empty();
-    submit_review(comment, experience, csrf_token, business_slug)
+    if (experience) {
+        comment = $('#comment').val();
+        csrf_token = $("input[name=csrfmiddlewaretoken]").val()
+        business_slug = $('#business_slug').val()
+        image_1 = document.getElementById('image_1').files[0];
+        image_2 = document.getElementById('image_2').files[0];
+        image_3 = document.getElementById('image_3').files[0];
+        var formData = new FormData();
+        formData.append("comment", comment);
+        formData.append("experience", experience);
+        formData.append('csrfmiddlewaretoken', csrf_token)
+        formData.append('business_slug', business_slug)
+        formData.append('image_1', image_1)
+        formData.append('image_2', image_2)
+        formData.append('image_3', image_3)
+        submit_review(formData)
+
+    } else {
+        $('#review_status').append('You must fill in the review section before submitting enter')
+    }
 
 })
 
-function submit_review(comment, experience, csrf_token, business_slug) {
+function submit_review(formdata) {
     $.ajax({
         type: "POST",
         url: '/mb/review/add-review/',
-        data: {
-            'comment': comment,
-            'experience': experience,
-            'csrfmiddlewaretoken': csrf_token,
-            'business_slug': business_slug,
-        },
+        data: formdata,
+        contentType: false,
+        processData: false,
+        cache: false,
+        enctype: 'multipart/form-data',
         success: function (response) {
             //if request if made successfully then the response represent the data
             if (response['status'] === 201) {
